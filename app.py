96c239d2e08bd8d006ce84a9d8d2d4349e314928
kumbharbherabhai <bharabhaikumbhar.actowiz@gmail.com>
2025-07-04 16:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from routes import register_routes
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
-register_routes(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/post", methods=["POST"])
+def receive_data():
+    try:
+        data = request.get_json()
+        return jsonify({"you_sent": data})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8005)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
